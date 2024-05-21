94caf74fbb816459101fd57cad92f3b3bf0eb43e
aehSolopov <solopal_aehit@students.vizja.pl>
2024-05-22 02:20:49 +0300

    add purchase form and API

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .routes import home, login_form, login, register, userprofile, get_user_data
+from .routes import home, login_form, login, register, userprofile, get_user_data, purchase_plan, purchase_form
 from .database import init_db
 
 app = Flask(__name__)
@@ -12,5 +12,8 @@ app.add_url_rule('/login', view_func=login, methods=['POST'])
 app.add_url_rule('/register', view_func=register, methods=['GET', 'POST'])
 app.add_url_rule('/userprofile', view_func=userprofile, methods=['GET'])
 app.add_url_rule('/get-user-data', view_func=get_user_data, methods=['GET'])
+app.add_url_rule('/purchase_form', view_func=purchase_form, methods=['GET'])
+app.add_url_rule('/purchase_plan', view_func=purchase_plan, methods=['POST'])
+
 
 init_db()  # Inicjalizacja bazy danych
