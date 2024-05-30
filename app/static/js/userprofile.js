40d3a5b23cdb19d6216566fb6a241334bba2e0d1
Bartosz Wilkowski <bartoszwilkowski3@gmail.com>
2024-05-30 19:04:30 +0200

    add docs for script.js, userprofile.js

--- app/static/js/userprofile.js
+++ app/static/js/userprofile.js
@@ -1,8 +1,25 @@
+/**
+ * @fileoverview This file contains the client-side JavaScript for handling user data retrieval and display on the web page.
+ * It uses jQuery for making AJAX requests to the server and updating the DOM with user information.
+ */
+
 $(document).ready(function () {
-    // AJAX request to get user data from the server
+    /**
+     * AJAX request to get user data from the server.
+     */
     $.ajax({
         url: '/get-user-data',
         type: 'GET',
+        /**
+         * Callback function for successful AJAX request.
+         * @param {Object} userData - The user data returned from the server.
+         * @param {string} userData.username - The username of the user.
+         * @param {string} userData.email - The email of the user.
+         * @param {string} userData.name - The first name of the user.
+         * @param {string} userData.surname - The surname of the user.
+         * @param {string} userData.plan_name - The subscription plan of the user.
+         * @param {Array<Object>} userData.payment_history - The payment history of the user.
+         */
         success: function (userData) {
             $('#username').text(userData.username);
             $('#email').text(userData.email);
@@ -24,6 +41,12 @@ $(document).ready(function () {
                 $('#paymentHistory').html('<p>No payment history available</p>');
             }
         },
+        /**
+         * Callback function for failed AJAX request.
+         * @param {Object} xhr - The XMLHttpRequest object.
+         * @param {string} status - The status of the request.
+         * @param {string} error - The error message.
+         */
         error: function (xhr, status, error) {
             console.error('Error:', error);
             if (xhr.status === 401) { // Check for unauthorized status
